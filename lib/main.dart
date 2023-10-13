0a02fd181d8e8408a8781956586a4102084fdf92
Z4SWALLOW <trisnawijaya003@gmal.com>
2023-10-13 14:53:05 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -118,10 +118,33 @@ class MyApp extends StatelessWidget {
                       BoxDecoration(color: Colors.red, shape: BoxShape.circle),
                 ),
               ],
-            )
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            CustomPaint(
+              size: Size(200, 200),
+              painter: segitiga(),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+class segitiga extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()..color = Colors.green;
+    final path = Path()
+      ..moveTo(size.width / 2, 0)
+      ..lineTo(size.width, size.height)
+      ..lineTo(0, size.height)
+      ..close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
